f602ed8b563019ea40bacaaa6661e6affca22ae1
raissahirose02 <raissamhirose@outlook.com>
2025-08-12 21:48:24 -0300

    Create exemplo01.c

---
+++ exemplo01.c
@@ -0,0 +1,16 @@
+/* 
+* Lab. 1
+* Exemplo
+*/ 
+
+#include <stdio.h>
+
+int main(){
+	printf("Mackenzie - FCI\n");
+	printf("Algoritmos e ");
+	printf("Prog. II\n");
+	printf("Turma: 02P\n");
+	
+	system("pause");
+	return 0;
+}
